61374c0e84054892da9e621785a0c59b4017e5a6
Marc Koppler <marc.koppler@gmail.com>
2017-10-03 09:52:28 +0200

    Hello World

---
+++ Programme/cmake-build-debug/CMakeFiles/Programme.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programme.dir/main.cpp.obj"
+  "Programme.pdb"
+  "Programme.exe"
+  "Programme.exe.manifest"
+  "libProgramme.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Programme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
